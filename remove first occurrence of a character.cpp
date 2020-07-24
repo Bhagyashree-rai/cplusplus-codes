9f6249e360be44045cff39882e1a3c0686ab1d5a
Bhagyashree-rai <68314938+Bhagyashree-rai@users.noreply.github.com>
2020-07-25 01:27:45 +0530

    Add files via upload

---
+++ remove first occurrence of a character.cpp
@@ -0,0 +1,44 @@
+                          
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int
+main ()
+
+{
+  
+char str[80];
+  
+char ch;
+  
+cin.getline (str, sizeof (str));
+  
+int n = sizeof (str);
+  
+cin >> ch;
+  int i = 0;
+  
+while (i < n && str[i] != ch)
+    
+i++;
+  
+while (i < n)
+    
+{
+      
+str[i] = str[i + 1];
+      
+i++;
+    
+}
+  
+cout << str;
+
+ 
+ return 0;
+
+}
