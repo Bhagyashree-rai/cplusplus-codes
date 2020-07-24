1e173f62fc6e135c69a0373e416d3bfd33fac685
Bhagyashree-rai <68314938+Bhagyashree-rai@users.noreply.github.com>
2020-07-25 01:48:17 +0530

    Add files via upload

---
+++ Remove all occurrence of a character.cpp
@@ -0,0 +1,57 @@
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int 
+main ()
+
+{
+  
+char str[80];
+  
+char ch;
+  
+cin.getline(str, sizeof (str));
+  
+int n = sizeof(str);
+  
+cin >> ch;
+  
+int i = 0, index;
+  
+while(i < n)
+ 
+   {
+      
+if (str[i] == ch)
+	
+{
+	 
+ index = i;
+	 
+ while (index < n)
+	   
+ {
+	
+      str[index] = str[index + 1];
+	
+      index++;
+	  
+  }
+
+  }
+    
+  i++;
+   
+ }
+  
+cout << str;
+
+ 
+ return 0;
+
+}
